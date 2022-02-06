7d5bbefa28656cabb66d5c8e43f9d9e1a08088a8
khoavuong <khoavuong@linecorp.com>
2022-02-06 11:45:49 +0700

    add credentials

--- api/common/constants.js
+++ api/common/constants.js
@@ -6,8 +6,8 @@ if (process.env.NODE_ENV && process.env.NODE_ENV === 'development') {
 exports.NODE_ENV = process.env.NODE_ENV || 'production';
 exports.PORT = process.env.PORT || 2000;
 exports.AWS_PROFILE = process.env.AWS_PROFILE || 'default';
-exports.PHOTOS_BUCKET = process.env.PHOTOS_BUCKET || '';
+exports.PHOTOS_BUCKET = process.env.PHOTOS_BUCKET || 'khoa-bucket-203';
 exports.DEFAULT_AWS_REGION = process.env.DEFAULT_AWS_REGION || 'ap-southeast-1';
 exports.SHOW_WARNINGS = process.env.SHOW_WARNINGS || 1;
-exports.SHOW_ADMIN_TOOLS = process.env.SHOW_ADMIN_TOOLS || 0;
+exports.SHOW_ADMIN_TOOLS = process.env.SHOW_ADMIN_TOOLS || 1;
 exports.TABLE_NAME = 'Employees';
